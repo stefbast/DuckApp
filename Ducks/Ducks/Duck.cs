4b4b7b4e0680226b0463054b2cb422a91ac64694
Stefaan Bastin <stefaan.bastin@gmail.com>
2018-04-15 23:29:44 +0200

    fix ignore file, add initial version

---
+++ Ducks/Ducks/Duck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ducks
+{
+    public class Duck
+    {
+        public void Quack()
+        {
+            Console.WriteLine("quack");
+        }
+    }
+}
---
+++ Ducks/Ducks/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Ducks
+{
+    class Program
+    {
+        static void Main()
+        {
+            var ducks = new List<Duck>
+            {
+                new Duck()
+            };
+
+            foreach (var duck in ducks)
+            {
+                duck.Quack();
+            }
+        }
+    }
+}
